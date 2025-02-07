b4301599ed3caeb445d96612c342f97ce4ae42c9
harrypeter07 <hassanmansuri570@gmail.com>
2025-02-07 17:34:19 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AuthForm from "@/components/AuthForm"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">IMPORTANT GOSSIP</h1>
+      <h1 className="text-4xl font-bold mb-8">IMPORTANT GOSSIPS</h1>
       <AuthForm />
     </main>
   )
