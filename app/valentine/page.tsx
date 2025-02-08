4511c4c27a90d7113925712114044093fd14ba91
Hassan Mansuri <146269350+harrypeter07@users.noreply.github.com>
2025-02-08 12:57:22 +0530

    Update page.tsx

--- app/valentine/page.tsx
+++ app/valentine/page.tsx
@@ -4,7 +4,7 @@ export default function ValentinePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-12">
         <p>
-            Happy Rose Day Chingiii 
+            Happy Rose Day Cupiee
         
         </p>
       <ValentineAnimation />
